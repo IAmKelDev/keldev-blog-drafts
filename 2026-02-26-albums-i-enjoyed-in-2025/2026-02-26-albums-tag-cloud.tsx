c08d26a899bb253f2666b68be7721c66952bddfa
Isaac Kellogg <keldtdev@gmail.com>
2026-02-26 11:18:36 -0500

    WIP Albums I Enjoyed: Clean up tag cloud code
    Fixed some formatting, added some comments, stuff like that.
    Better typedef for tag cloud react component.
    Made tag min and max size specifiable via props. Old hard-coded values are now just defaults.
    Max tag count is now only calculated once.
    
    Slightly reformatted the HTML output. Fewer unnecessary divs. One more line break.

--- 2026-02-26-albums-i-enjoyed-in-2025/2026-02-26-albums-tag-cloud.tsx
+++ 2026-02-26-albums-i-enjoyed-in-2025/2026-02-26-albums-tag-cloud.tsx
@@ -1,6 +1,20 @@
+/**
+ * Component to display a tag cloud of album tags. Clicking on a tag will show a list of albums that have that tag.
+ * The size of the tag is determined by how many albums have that tag.
+ * 
+ * Expects a list of AlbumTag objects, see below.
+ */
+
 import React from "react";
 import "./2026-02-26-albums-tag-cloud.css";
 
+/**
+ * 
+ * link: string - link to the album (can be relative link to jump on the page)
+ * albumTitle: string - title of the album
+ * albumArtist: string - artist of the album
+ * tags: string[] - list of tags associated with the album
+ */
 type AlbumTag = {
     link: string;
     albumTitle: string;
@@ -8,7 +22,19 @@ type AlbumTag = {
     tags: string[];
 }
 
-const AlbumsTagCloud: React.FC<{ tags: AlbumTag[] }> = ({ tags }) => {
+/**
+ * tags: list of AlbumTag objects
+ * maxTagSize: maximum font size for the most common tag (default: 38px)
+ * minTagSize: minimum font size for the least common tag (default: 12px)
+ */
+type AlbumsTagCloudProps = {
+    tags: AlbumTag[];
+    maxTagSize?: number;
+    minTagSize?: number;
+}
+
+const AlbumsTagCloud: React.FC<AlbumsTagCloudProps> = ({ tags, maxTagSize = 38, minTagSize = 12 }) => {
+
     //count instances of tags
     const tagCounts: Record<string, number> = {};
     tags.forEach(tag => {
@@ -16,48 +42,51 @@ const AlbumsTagCloud: React.FC<{ tags: AlbumTag[] }> = ({ tags }) => {
             tagCounts[tagName] = (tagCounts[tagName] || 0) + 1;
         });
     });
+
     //Sort alphabetically
-    const sortedTags = Object.entries(tagCounts)
+    const sortedTagsWithCounts = Object.entries(tagCounts)
         .sort()
-        .map(([tag, count]) => ({ tag, count }));
+        .map(([tag, count]) => ({ tag, count })); //Attach counts
 
-    const maxTagSize = 38; // max font size for the most frequent tag
-    const minTagSize = 12; // min font size for the least frequent tag
+    const maxCount = Math.max(...sortedTagsWithCounts.map(t => t.count));
 
+    /*
+     *  Calculate the font size for a tag based on its count.
+     *  The most common tag will have the maxTagSize, and the least common tag will have the minTagSize.
+     *  The size is calculated linearly based on the count relative to the maximum count.
+     */
     const getTagSize = (count: number) => {
-        const maxCount = Math.max(...sortedTags.map(t => t.count));
         return minTagSize + (count / maxCount) * (maxTagSize - minTagSize);
     };
 
     const [selectedTag, setSelectedTag] = React.useState<string | null>(null);
 
-  return (
-    <>
-        <div className="album-tag-cloud">
-            {sortedTags.map((tag, index) => (
-            <span key={index} className="album-tag" style={{ fontSize: `${getTagSize(tag.count)}px` }} onClick={() => setSelectedTag(tag.tag)}>
-                {tag.tag}<span style={{ fontSize: `${minTagSize}px` }}> ({tag.count})</span>
-            </span>
-            ))}
-        </div>
-        <div>
+    // Tag cloud on top, and a list of albums that have the selected tag below (when a tag is clicked)
+    return (
+        <>
+            <div className="album-tag-cloud">
+                {sortedTagsWithCounts.map((tag, index) => (
+                    <span key={index} className="album-tag" style={{ fontSize: `${getTagSize(tag.count)}px` }} onClick={() => setSelectedTag(tag.tag)}>
+                        {tag.tag}<span style={{ fontSize: `${minTagSize}px` }}> ({tag.count})</span>
+                    </span>
+                ))}
+            </div>
             {selectedTag && (
                 <>
-                <br />
-                <div>
-                    <h3>Albums tagged "{selectedTag}"</h3>
-                    <ol>
-                        {tags.filter(album => album.tags.includes(selectedTag)).map((album, index) => (
-                            <li key={index}>
-                                <a href={album.link}><i>{album.albumTitle}</i> ({album.albumArtist})</a> - {album.tags.sort().join(', ')}
-                            </li>
-                        ))}
-                    </ol>
-                </div>
+                    <br /> <br />
+                    <div>
+                        <h3>Albums tagged "{selectedTag}"</h3>
+                        <ol>
+                            {tags.filter(album => album.tags.includes(selectedTag)).map((album, index) => (
+                                <li key={index}>
+                                    <a href={album.link}><i>{album.albumTitle}</i> ({album.albumArtist})</a> - {album.tags.sort().join(', ')}
+                                </li>
+                            ))}
+                        </ol>
+                    </div>
                 </>
             )}
-        </div>
-    </>
+        </>
     );
 };
 
